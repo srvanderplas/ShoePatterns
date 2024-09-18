8cf88d44d984e703fa62d48e09ac9351dfc423ee
Hiiiua <mhua4816@gmail.com>
2024-09-17 20:48:40 -0500

    Add circle, ellipse and path Arc. large_arc and sweep flags are only for ellipse and fixed. If we need other paths, modification is needed.

--- CreatePatternXml/ele2Draw.py
+++ CreatePatternXml/ele2Draw.py
@@ -1,27 +1,20 @@
-import svgelements as element
+import svgelements as elements
 import drawsvg as draw
-# element object
+import math
+import numpy as np
+
 def eleObjType(ele):
     import svgelements
-    """object from element, always in a group"""
     if isinstance(ele, svgelements.Group):
-        if len(ele)>1:
-                return "Group"
-        else:
-            ele = ele[0]
-            if isinstance(ele, svgelements.Path):
-                return "Path"
-            if isinstance(ele, svgelements.Circle):
-                return "Circle"
-            if isinstance(ele, svgelements.Rect):
-                return "Rect"
-    else:
-        if isinstance(ele, svgelements.Path):
-            return "Path"
-        if isinstance(ele, svgelements.Circle):
-            return "Circle"
-        if isinstance(ele, svgelements.Rect):
-            return "Rect"
+        return "Group"
+    if isinstance(ele, svgelements.Path):
+        return "Path"
+    if isinstance(ele, svgelements.Circle):
+        return "Circle"
+    if isinstance(ele, svgelements.Rect):
+        return "Rect"
+    if isinstance(ele, svgelements.Ellipse):
+        return "Ellipse"
 
 def elePathType(elePath):
     import svgelements as svgelement
@@ -33,26 +26,53 @@ def elePathType(elePath):
         return "CubicBezier"
     if isinstance(elePath, svgelement.svgelements.QuadraticBezier):
         return "QuadraticBezier"
-    if isinstance(elePath, svgelement.svgelements.Close):
+    if isinstance(elePath, svgelements.svgelements.Close):
         return "Close"
+    if isinstance(elePath, svgelements.svgelements.Arc):
+        return "Arc"
 
+def arc_rotation(EleArc):
+    """For arc objext only, calculate the rotation angle of the arc"""
+    if elePathType(EleArc) == "Arc": # if isinstance(arc[1], svgelements.svgelements.Arc):
+        v = EleArc.center - EleArc.start
+        return math.degrees(math.atan2(v.x, v.y))
+    else:
+        raise Exception("Not an svgelements Arc object in path") 
+    
 # element to draw
 # ele为单个元素
-def eleone2draw(ele):
-    import svgelements as element
-    import drawsvg as draw
+def eleone2Draw(ele):
     eleType = eleObjType(ele)
     if eleType == "Circle":
-        pass #等circle的时候再写
+        drawCircle =draw.Circle(ele.cx, ele.cy, ele.implicit_r,
+                        fill=ele.fill if hasattr(ele, "fill") else "none",
+                        stroke=ele.stroke if hasattr(ele, "stroke") else "black",
+                        stroke_width=ele.stroke_width if hasattr(ele, "stroke_width") else 1,
+                        id = ele.id if hasattr(ele, "id") else "circle",
+                        transform = ele.transform if hasattr(ele, "transform") else elements.Matrix(1, 0, 0, 1, 0, 0),
+                        stroke_dasharray = ele.values["stroke-dasharray"] if "stroke-dasharray" in ele.values.keys() else "none",
+                        stoke_opacity = ele.values["stroke-opacity"] if "stroke-dasharray" in ele.values.keys() else "none")
+        return drawCircle
+    if eleType == "Ellipse":
+        drawEllipse = draw.Ellipse(ele.cx, ele.cy, ele.rx, ele.ry,
+                         fill=ele.fill if hasattr(ele, "fill") else "none",
+                        stroke=ele.stroke if hasattr(ele, "stroke") else "black",
+                        stroke_width=ele.stroke_width if hasattr(ele, "stroke_width") else 1,
+                        id = ele.id if hasattr(ele, "id") else "ellipse",
+                        transform = ele.transform if hasattr(ele, "transform") else elements.Matrix(1, 0, 0, 1, 0, 0),
+                        stroke_dasharray = ele.values["stroke-dasharray"] if "stroke-dasharray" in ele.values.keys() else "none",
+                        stoke_opacity = ele.values["stroke-opacity"] if "stroke-dasharray" in ele.values.keys() else "none")
+        return drawEllipse
     if eleType == "Path":
         drawPath = draw.Path(stroke = ele.stroke if hasattr(ele, "stroke") else "black",
                             stroke_width = ele.stroke_width if hasattr(ele, "stroke_width") else 1,
                             fill = ele.fill if hasattr(ele, "fill") else "none",
                             fill_opacity = ele.fill_opacity if hasattr(ele, "fill_opacity") else 1,
                             id = ele.id if hasattr(ele, "id") else "path",
-                            transform = ele.transform if hasattr(ele, "transform") else element.Matrix(1,0,0,1,0,0))
-                            
-        
+                            transform = ele.transform if hasattr(ele, "transform") else elements.Matrix(1, 0, 0, 1, 0, 0),
+                            stroke_dasharray = ele.values["stroke-dasharray"] if "stroke-dasharray" in ele.values.keys() else "none",
+                            stoke_opacity = ele.values["stroke-opacity"] if "stroke-dasharray" in ele.values.keys() else "none"
+                            )
         for elePath in ele:
             elePType = elePathType(elePath)
             if elePType == "Move":
@@ -66,32 +86,61 @@ def eleone2draw(ele):
             if elePType == "Close":
                 drawPath.Z()
             if elePType == "CubicBezier":
-                drawPath.C(elePath.x1, elePath.y1, elePath.x2, elePath.y2, elePath.x, elePath.y)
+                drawPath.C(elePath.control1.x, elePath.control1.y, elePath.control2.x, elePath.control2.y, elePath.end.x, elePath.end.y)
             if elePType == "QuadraticBezier":
-                drawPath.Q(elePath.x1, elePath.y1, elePath.x, elePath.y)
+                drawPath.Q(elePath.control1.x, elePath.control1.y, elePath.end.x, elePath.end.y)
+            if elePType == "Arc": # for ellipse ONLY, otherwise, need work on large_arc, sweep flags
+                drawPath.A(rx=elePath.rx, 
+                    ry=elePath.ry,
+                    large_arc = 0,
+                    rot = elePath.get_rotation()*360/np.pi/2, 
+                    sweep = 1, 
+                    ex = elePath.end.x, 
+                    ey = elePath.end.y)
         return drawPath
+    
     if eleType == "Rect":
         drawRect = draw.Rectangle(ele.x, ele.y, ele.width, ele.height, 
-                                fill=ele.fill if hasattr(ele, "fill") else "none",
-                                stroke=ele.stroke if hasattr(ele, "stroke") else "black",
-                                stroke_width=ele.stroke_width if hasattr(ele, "stroke_width") else 1,
-                                id = ele.id if hasattr(ele, "id") else "rect",
-                                transform = ele.transform if hasattr(ele, "transform") else element.Matrix(1,0,0,1,0,0))
-            
+                        fill=ele.fill if hasattr(ele, "fill") else "none",
+                        stroke=ele.stroke if hasattr(ele, "stroke") else "black",
+                        stroke_width=ele.stroke_width if hasattr(ele, "stroke_width") else 1,
+                        id = ele.id if hasattr(ele, "id") else "rect",
+                        transform = ele.transform if hasattr(ele, "transform") else elements.Matrix(1, 0, 0, 1, 0, 0),
+                        stroke_dasharray = ele.values["stroke-dasharray"] if "stroke-dasharray" in ele.values.keys() else "none",
+                        stoke_opacity = ele.values["stroke-opacity"] if "stroke-dasharray" in ele.values.keys() else "none"
+            )
         return drawRect
 
 
 
-def ele2draw(eles):
+def ele2Draw(eles):
     eleType = eleObjType(eles) #多元素，group
     if eleType == "Group":
         drawGroup = draw.Group(id = eles.id if hasattr(eles, "id") else "group",
-                               transform = eles.transform if hasattr(eles, "transform") else element.Matrix(1,0,0,1,0,0))
-                               
+                               transform = eles.transform if hasattr(eles, "transform") else elements.Matrix(1, 0, 0, 1, 0, 0)
+                               )
         for ele in eles:
-            drawEle = eleone2draw(ele)
+            drawEle = ele2Draw(ele)
             drawGroup.append(drawEle)
         return drawGroup
     else:
-        ele = eles[0]
-        return eleone2draw(ele)
+        return eleone2Draw(eles)
+    
+if __name__ == "__main__":
+    import svgelements as svgelements
+    import drawsvg as draw
+    # read the SVG file
+    svg = svgelements.SVG.parse("rec.svg")
+
+    d00 = draw.Drawing(1500, 1500)
+    ele0 = [i for i in svg.elements()][0]
+    for i in range(len(ele0)):
+        if i==0:
+            pass
+        d00.append(ele2Draw(ele0[i]))
+    # d00.save_png("")
+    # d00.rasterize()
+    # d00.save_png("demo.png")
+    d00
+    
+    
